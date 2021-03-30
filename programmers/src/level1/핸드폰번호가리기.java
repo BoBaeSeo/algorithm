6d9b1d0d3b53dc02d010809f060e837cc8ae37e3
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-03-30 23:12:56 +0900

    Programmers level1
    핸드폰 번호 가리기 complete

--- programmers/src/level1/핸드폰번호가리기.java
+++ programmers/src/level1/핸드폰번호가리기.java
@@ -3,7 +3,12 @@ package level1;
 public class 핸드폰번호가리기 {
 
 	public String solution(String phone_number) {
-        String answer = "";
+		String num = phone_number.substring(phone_number.length()-4, phone_number.length());
+		String answer = "";
+		for(int i = 0; i < phone_number.length()-4; i++) {
+			answer += "*";
+		}
+		answer += num;
         return answer;
     }
 	
