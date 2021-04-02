83b99e56a9db2919d4d2bf0016e329526e3dc16c
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-02 17:01:19 +0900

    Backjoon
    개 출력 complete

---
+++ baekjoon/src/inoutput/개.java
@@ -0,0 +1,12 @@
+package inoutput;
+
+public class 개 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|*/");
+	}
+}
