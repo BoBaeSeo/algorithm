8295ccc7f5df486caa5beb23b132351db25c76ee
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-03 23:08:57 +0900

    backjoon
    곱셈 complete

---
+++ baekjoon/src/inoutput/곱셈.java
@@ -0,0 +1,17 @@
+package inoutput;
+
+import java.util.Scanner;
+
+public class 곱셈 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a * (b % 10));
+		System.out.println(a * ((b % 100) / 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+	}
+}
