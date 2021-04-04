d5bdb8caa0ce6a760e3a71a158d6b32c5e88b8f9
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-04 19:38:30 +0900

    Backjoon
    사분면 고르기 complete

---
+++ baekjoon/src/stpe2if/사분면고르기.java
@@ -0,0 +1,23 @@
+package stpe2if;
+
+import java.util.Scanner;
+
+public class 사분면고르기 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		
+		if(x > 0 && y > 0) {
+			System.out.println(1);
+		} else if(x < 0 && y > 0) {
+			System.out.println(2);
+		} else if(x < 0 && y < 0) {
+			System.out.println(3);
+		} else {
+			System.out.println(4);
+		}
+	}
+}
