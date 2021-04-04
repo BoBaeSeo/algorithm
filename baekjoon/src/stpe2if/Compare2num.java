5cc9680a30e09f8e3a0afaea7ce11d77bd5a4c1b
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-04 19:19:28 +0900

    Backjoon
    두 수 비교하기 complete

---
+++ baekjoon/src/stpe2if/Compare2num.java
@@ -0,0 +1,21 @@
+package stpe2if;
+
+import java.util.Scanner;
+
+public class Compare2num {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		if(a > b) {
+			System.out.println(">");
+		} else if(a < b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+}
