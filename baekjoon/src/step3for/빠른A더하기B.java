9b3c0bc49a7ddff178a631c33804fcc4f2bb649f
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-05 21:08:36 +0900

    Backjoon
    빠른 A+B complete

---
+++ baekjoon/src/step3for/빠른A더하기B.java
@@ -0,0 +1,32 @@
+package step3for;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 빠른A더하기B {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			StringTokenizer st;
+			int n = Integer.parseInt(br.readLine());
+			
+			String result = "";
+			for(int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				bw.write(a + b + "\n");
+			}
+			br.close();
+			bw.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
