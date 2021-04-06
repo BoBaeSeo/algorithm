c3db6f8cea9fca4c15cd79599108356d09db7b24
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-06 15:38:42 +0900

    Baekjoon
    더하기 사이클 complete

---
+++ baekjoon/src/step3for/더하기사이클.java
@@ -0,0 +1,26 @@
+package step3for;
+
+import java.util.Scanner;
+
+public class 더하기사이클 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int temp = n;
+		int count = 0;
+		
+		while(true) {
+			int left = temp / 10;
+			int right = temp % 10;
+			temp = right * 10 + (left + right) % 10;
+			count++;
+			if(n == temp) {
+				break;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
