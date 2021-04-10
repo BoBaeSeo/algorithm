a8de693084e299473836c730799131bc72c0029a
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-10 14:35:27 +0900

    Baekjoon
    OX 퀴즈 complete

---
+++ baekjoon/src/arrayAndFunction/OX퀴즈.java
@@ -0,0 +1,26 @@
+package arrayAndFunction;
+
+import java.util.Scanner;
+
+public class OX퀴즈 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			char[] arr = scan.next().toCharArray();
+			int num = 0, sum = 0;;
+			for(char c : arr) {
+				if(c == 'O') {
+					num++;
+					sum += num;
+				} else {
+					num = 0;
+				}
+			}
+			System.out.println(sum);
+		}
+		
+	}
+}
