72c3847a6340ff20741a09ba61c1c9b1f1158566
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-04-13 12:16:57 +0900

    Baekjoon
    수 정렬하기 complete

---
+++ baekjoon/src/BOJguide_ryute/수정렬하기.java
@@ -0,0 +1,27 @@
+package BOJguide_ryute;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 수정렬하기 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i : arr) {
+			sb.append(i).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
