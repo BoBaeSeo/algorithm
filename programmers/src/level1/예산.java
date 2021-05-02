c111b3f679a2a0b1318a68b11d42dfc63685497d
BoBaeSeo <69306907+BoBaeSeo@users.noreply.github.com>
2021-05-02 13:31:40 +0900

    programmers level1
    예산 complete

---
+++ programmers/src/level1/예산.java
@@ -0,0 +1,17 @@
+package level1;
+
+import java.util.Arrays;
+
+public class 예산 {
+	public int solution(int[] d, int budget) {
+		int answer = 0;
+		Arrays.sort(d);
+		for(int i = 0; i < d.length; i++) {
+			if(budget - d[i] < 0) break;
+			budget -= d[i];
+			answer++;
+		}
+        return answer;
+    }
+
+}
